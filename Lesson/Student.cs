7ac88ee89eef47e056e765f3601bd4bef2d83a7e
Ефимов Максим <efimov_m@itstep.academy>
2023-09-12 20:13:37 +0300

    1

--- Lesson/Student.cs
+++ Lesson/Student.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Lesson
 {
-    internal class Student
+    class Student
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public StudentCard StudentCard { get; set; }
     }
 }
